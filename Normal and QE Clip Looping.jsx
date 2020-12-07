5a4f29032f126095692513108808f010118a39ed
Nate Lovell <Natelovell99@gmail.com>
2020-12-06 21:40:43 -0700

    Create Normal and QE Clip Looping.jsx
    main code

---
+++ Normal and QE Clip Looping.jsx
@@ -0,0 +1,20 @@
+﻿$.writeln("--------------------------");
+// Normal track looping
+var project = app.project;
+var sequence = project.activeSequence;
+$.writeln("Normal Track Loop Start");
+for(var i = 0; i < sequence.videoTracks[1].clips.numItems; i++) {
+    $.writeln("i = " + i);
+    $.writeln(sequence.videoTracks[1].clips[i].name);
+    }
+$.writeln("Normal Track Loop End");
+
+// QE track looping
+app.enableQE();
+$.writeln("QE Track Loop Start");
+var thisTrack = qe.project.getActiveSequence().getVideoTrackAt(1);
+for(var i = 0; i < thisTrack.numItems; i++) {
+    $.writeln("i = " + i);
+    $.writeln(thisTrack.getItemAt(i).name);
+    }
+$.writeln("QE Track Loop End");
